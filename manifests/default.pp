d302d4413f56fb22678dcd87ef7fb27d18c80be6
jsun9406 <jasons@iqmetrix.com>
2021-12-22 10:23:06 -0800

    add

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,11 +3,23 @@ node 'ansible-agent01' {
 }
 
 node 'win-9kfma7hmroh' {
-  exec { 'stop-newrelicservice':
-    command   => 'Stop-Service newrelic-infra -Force -PassThru',
-    unless    => '$service = Get-Service -Name newrelic-infra -ErrorAction SilentlyContinue;
-                  if ($service -eq $null) {Exit1}',
+
+  exec { 'Check-Service':
+    command   => '',
+    onlyif    => 'if (!(Get-Service -Name newrelic-infra -ErrorAction SilentlyContinue)){exit 1}',
     provider  => powershell,
     logoutput => true,
+    before    => DSC['stop-newrelicservice']
+  }
+
+  dsc {'stop-newrelicservice':
+    resource_name => 'Service',
+    module => 'PSDesiredStateConfiguration',
+    properties => {
+      name          => 'newrelic-infra',
+      ensure          => 'present',
+      startupType   => 'Disabled',
+      state         => 'Stopped',
+    }
   }
 }
