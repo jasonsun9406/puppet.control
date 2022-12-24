33a1fe3ef381338d98819deb73594e95dc7871f5
jsun9406 <jasons@iqmetrix.com>
2022-12-23 22:24:57 -0800

    add

--- modules/profile/manifests/common/dns.pp
+++ modules/profile/manifests/common/dns.pp
@@ -1,6 +1,6 @@
 class profile::common::dns {
   class { 'resolv_conf':
-    nameservers => ['192.168.1.210'],
+    nameservers => ['192.168.1.210','192.168.1.254'],
     searchpath  => ['toolr.local'],
   }
 }
