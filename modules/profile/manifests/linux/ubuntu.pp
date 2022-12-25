56ef8681c4aab83121ff1a9194e9b9833bb6396b
jsun9406 <jasons@iqmetrix.com>
2022-12-25 01:25:47 -0800

    add

--- modules/profile/manifests/linux/ubuntu.pp
+++ modules/profile/manifests/linux/ubuntu.pp
@@ -8,30 +8,31 @@ class profile::linux::ubuntu {
     domain               => "$::domain",
     domain_join_user     => "$adcomputerjoinaccountname",
     domain_join_password => "$adcomputerjoinaccountpassword",
-    # manage_sssd_config => true,
-    # sssd_config        => {
-    #   'sssd' => {
-    #     'domains'             => "$::domain",
-    #     'config_file_version' => '2',
-    #     'default_domain_suffix' => "$::domain",
-    #     'services'            => 'nss,pam',
-    #   },
-    #   "domain/${::domain}" => {
-    #
-    #     'ad_domain'                      => "$::domain",
-    #     'krb5_realm'                     => upcase($::domain),
-    #     'realmd_tags'                    => 'manages-system joined-with-adcli',
-    #     'cache_credentials'              => 'True',
-    #     'id_provider'                    => 'ad',
-    #     'access_provider'                => 'ad',
-    #     'krb5_store_password_if_offline' => 'True',
-    #     'default_shell'                  => '/bin/bash',
-    #     'ldap_id_mapping'                => 'True',
-    #     'fallback_homedir'               => '/home/%u',
-    #     'simple_allow_groups' => $allowgroups,
-    #     'sudo_provider' => 'ad',
-    #     'use_fully_qualified_names' => 'True',
-    #   },
+    computer_name        => upcase($::domain),
+    manage_sssd_config => true,
+    sssd_config        => {
+      'sssd' => {
+        'domains'             => "$::domain",
+        'config_file_version' => '2',
+        'default_domain_suffix' => "$::domain",
+        'services'            => 'nss,pam',
+      },
+      "domain/${::domain}" => {
+
+        'ad_domain'                      => "$::domain",
+        'krb5_realm'                     => upcase($::domain),
+        'realmd_tags'                    => 'manages-system joined-with-adcli',
+        'cache_credentials'              => 'True',
+        'id_provider'                    => 'ad',
+        'access_provider'                => 'ad',
+        'krb5_store_password_if_offline' => 'True',
+        'default_shell'                  => '/bin/bash',
+        'ldap_id_mapping'                => 'True',
+        'fallback_homedir'               => '/home/%u',
+        'simple_allow_groups' => $allowgroups,
+        'sudo_provider' => 'ad',
+        'use_fully_qualified_names' => 'True',
+      },
     }
   }
 
