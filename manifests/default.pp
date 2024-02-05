d447bf7ad10f2c14a6c9c9b5ba20d04dc86f9429
jsun9406 <jasons@iqmetrix.com>
2024-02-05 14:10:36 -0800

    test

--- manifests/default.pp
+++ manifests/default.pp
@@ -7,8 +7,7 @@ node 'ansible-agent01' {
 
 node 'win-miha0ugrs92' {
   pspackageprovider {'Nuget':
-    ensure   => 'present',
-    provider => 'windowspowershell',
+    ensure   => 'present'
   }
   package { 'Az':
     ensure => '11.2.0',
